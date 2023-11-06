fce00b2367b33bce52b9842bf1f348a9550c2ca1
Brian-Wang6 <brian.wangww@gmail.com>
2023-11-06 15:34:00 +0800

    fix bug - change text of one feature, all features changed

--- ProcessImageWithOpenLayer/NpmJs/src/FeatureStyle.js
+++ ProcessImageWithOpenLayer/NpmJs/src/FeatureStyle.js
@@ -1,16 +1,6 @@
 ﻿import { Style, Fill, Text, Stroke } from 'ol/style.js';
 class FeatureStyle {
-    label = new Text({
-        font: '26px Calibri,sans-serif',
-        fill: new Fill({
-            color: '#000',
-        }),
-        stroke: new Stroke({
-            color: '#fff',
-            width: 4,
-        }),
-    });
-
+    
     setfeaturezindex(feature, zindex) {
         if (feature === undefined)
             return;
@@ -26,8 +16,18 @@ class FeatureStyle {
             return;
         var style = feature.getStyle();
         if (style !== undefined) {
-            this.label.setText(text);
-            style.setText(this.label);
+            var label = new Text({
+                font: '26px Calibri,sans-serif',
+                fill: new Fill({
+                    color: '#000',
+                }),
+                stroke: new Stroke({
+                    color: '#fff',
+                    width: 4,
+                }),
+            });
+            label.setText(text);
+            style.setText(label);
             feature.changed();
         }
     }
